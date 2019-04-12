14fd6d82a8e8a2e59a196f50349e4ae9915b0965
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-04-12 15:14:49 +0200

    User model created

---
+++ src/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  analytics: {
+    ipAddressses: [{}],
+    productsReaded: [{}],
+    bacodesFailed: [{}],
+    querySeraches: [{}],
+    productsViewed: [{}],
+  },
+  username: String,
+  email: String,
+  devices: [{
+    _id: Schema.Types.ObjectId,
+    manufacturer: String,
+    brand: String,
+    model: String,
+    carrier: String,
+    platform: String,
+    systemVersion: String,
+    appVersion: String,
+  }],
+})
+
+module.exports = mongoose.model('user', userSchema)
