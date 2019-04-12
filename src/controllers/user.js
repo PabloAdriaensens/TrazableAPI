7156600e8bef5f9e3aa7c8c15cd07a808cfa2157
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-04-13 00:23:40 +0200

    top ten products most viewed

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -31,4 +31,32 @@ module.exports = {
 
     res.status(200).json(resp)
   },
+
+  getProductsViewed: async (req, res, next) => {
+    const results = await User.aggregate([{
+      $unwind: '$analytics',
+    },
+    {
+      $unwind: '$analytics.productsViewed',
+    },
+    {
+      $group: {
+        _id: '$analytics.productsViewed.product',
+        total: {
+          $sum: 1,
+        },
+      },
+    },
+    {
+      $sort: {
+        'total': -1,
+      },
+    },
+    {
+      $limit: 10,
+    },
+    ])
+
+    return res.status(200).json(results)
+  },
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,10 +4,13 @@ const router = express.Router()
 const {
   index,
   getBrand,
+  getProductsViewed,
 } = require('../controllers/user')
 
 router.get('/', index)
 
 router.get('/devices', getBrand)
+
+router.get('/productsViewed', getProductsViewed)
 
 module.exports = router
